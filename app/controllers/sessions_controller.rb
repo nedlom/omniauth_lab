d04513fc9ccdcd2a8f9ccff9198d0930580a683c
Nick <nelom3@gmail.com>
2021-09-17 12:25:05 -0700

    commit tree ef753c981e57dfeb9cd6dd44f93267d464cefe20

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,7 +3,7 @@ class SessionsController < ApplicationController
   # strategy. We'll remove this when we move to a "real" provider.
   skip_before_action :verify_authenticity_token, only: :create
 
-  def xcreate
+  def create
     # After entering a name and email value in the /auth/developer
     # path and submitting the form, you will see a pretty-print of
     # the authentication data object that comes from the "developer"
@@ -14,10 +14,11 @@ class SessionsController < ApplicationController
     # We're going to save the authentication information in the session
     # for demonstration purposes. We want to keep this data somewhere so that,
     # after redirect, we have access to the returned data
-    session[:name] = request.env['omniauth.auth']['info']['nick-name']
+    session[:name] = request.env['omniauth.auth']['info']['qnickname']
     session[:omniauth_data] = request.env['omniauth.auth']
-
     # Ye olde redirect
+    
     redirect_to root_path
+    
   end
 end
